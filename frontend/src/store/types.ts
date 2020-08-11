f7eb6b7dce5c38a6f266125685695ac51ff01f8c
Michelon Souza <michelon.souza@hotmail.com>
2020-08-11 00:31:05 -0300

    fix: store types add "index"

--- frontend/src/store/types.ts
+++ frontend/src/store/types.ts
@@ -1,13 +1,7 @@
-// @PRODUCT_LIST
-export const PRODUCT_LIST_REQUEST = '@productList/PRODUCT_LIST_REQUEST';
-export const PRODUCT_LIST_SUCCESS = '@productList/PRODUCT_LIST_SUCCESS';
-export const PRODUCT_LIST_FAIL = '@productList/PRODUCT_LIST_FAIL';
+import * as cartTypes from './modules/cart/types';
+import * as categoriesTypes from './modules/categories/types';
 
-// @PRODUCT
-export const PRODUCT_REQUEST = '@product/PRODUCT_REQUEST';
-export const PRODUCT_SUCCESS = '@product/PRODUCT_SUCCESS';
-export const PRODUCT_FAIL = '@product/PRODUCT_FAIL';
-
-// @CART
-export const ADD_TO_CART = '@cart/ADD_TO_CART';
-export const REMOVE_FROM_CART = '@cart/REMOVE_FROM_CART';
+export default {
+  cartTypes,
+  categoriesTypes,
+};
